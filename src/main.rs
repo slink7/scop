870c3a996f749cebd95d4d944c6fc97c3a8e5a3a
Sebastien Cambier <scambier@student.42.fr>
2026-09-21 19:11:51 +0200

    Added custom log callback

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,31 @@ const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
 const VALIDATION_LAYER: vk::ExtensionName = 
     vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
 
-unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+extern "system" fn debug_callback(
+    severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    type_: vk::DebugUtilsMessageTypeFlagsEXT,
+    data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _: *mut c_void
+) -> vk::Bool32 {
+    let data = unsafe { *data };
+    let message = unsafe { CStr::from_ptr(data.message) }.to_string_lossy();
+
+    if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR {
+        error!("({:?}) {}", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
+        warn!("({:?}) {}", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO {
+        debug!("({:?}) {}", type_, message);
+    } else {
+        debug!("({:?}) {}", type_, message);
+    }
+
+    vk::FALSE
+}
+
+unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData)
+    -> Result<Instance>
+{
     let application_info = vk::ApplicationInfo::builder()
         .application_name(b"scop")
         .application_version(vk::make_version(1, 0, 0))
@@ -59,6 +83,10 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
         .map(|e| e.as_ptr())
         .collect::<Vec<_>>();
 
+    if VALIDATION_ENABLED {
+        extensions.push(vk::EXT_DEBUG_UTILS_EXTENSION.name.as_ptr());
+    }
+
     let flags = if
         cfg!(target_os = "macos") &&
         entry.version()? >= PORTABILITY_MACOS_VERSION
@@ -77,7 +105,22 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
         .enabled_extension_names(&extensions)
         .flags(flags);
 
-    Ok(entry.create_instance(&info, None)?)
+    let instance = entry.create_instance(&info, None)?;
+
+    if VALIDATION_ENABLED {
+        let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+            .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL |
+                vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION |
+                vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+            )
+            .user_callback(Some(debug_callback));
+
+        data.messenger = instance.create_debug_utils_messenger_ext(&debug_info, None)?;
+    }
+
+    Ok(instance)
 }
 
 fn main() -> Result<()> {
@@ -116,6 +159,7 @@ fn main() -> Result<()> {
 struct App {
     entry: Entry,
     instance: Instance,
+    data: AppData,
     it: i32
 }
 
@@ -124,9 +168,10 @@ impl App {
     unsafe fn create(window: &Window) -> Result<Self> {
         let loader = LibloadingLoader::new(LIBRARY)?;
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
-        let instance = create_instance(window, &entry)?;
+        let mut data = AppData::default();
+        let instance = create_instance(window, &entry, &mut data)?;
 
-        Ok(Self { entry, instance, it: 5 })
+        Ok(Self { entry, instance, data, it: 0 })
     }
 
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
@@ -136,10 +181,16 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) {
+        if VALIDATION_ENABLED {
+            self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
+        }
+
         self.instance.destroy_instance(None);
     }
 
 }
 
 #[derive(Clone, Debug, Default)]
-struct AppData {}
+struct AppData {
+    messenger: vk::DebugUtilsMessengerEXT
+}
