cc922d4eb97d1e4469b05ad42e5910cfd1fba7ec
Sebastien Cambier <scambier@student.42.fr>
2026-09-21 23:09:23 +0200

    Added logical device

--- src/main.rs
+++ src/main.rs
@@ -232,11 +232,50 @@ impl QueueFamilyIndicies {
     }
 }
 
+unsafe fn create_logical_device(entry: &Entry, instance: &Instance, data: &mut AppData)
+    -> Result<Device> {
+    let indices = QueueFamilyIndicies::get(instance, data, data.physical_device)?;
+
+    let queue_priorities = &[1.0];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    let layers = if VALIDATION_ENABLED {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        vec![]
+    };
+
+    let mut extensions = vec![];
+
+    if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+        extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
+    }
+
+    let features = vk::PhysicalDeviceFeatures::builder();
+    
+    let queue_infos = &[queue_info];
+
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
+}
+
 #[derive(Clone, Debug)]
 struct App {
     entry: Entry,
     instance: Instance,
     data: AppData,
+    device: Device,
     it: i32
 }
 
@@ -250,7 +289,9 @@ impl App {
 
         pick_physical_device(&instance, &mut data)?;
 
-        Ok(Self { entry, instance, data, it: 0 })
+        let device = create_logical_device(&entry, &instance, &mut data)?;
+
+        Ok(Self { entry, instance, data, device, it: 0 })
     }
 
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
@@ -262,6 +303,8 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) {
+        self.device.destroy_device(None);
+
         if VALIDATION_ENABLED {
             self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
@@ -274,5 +317,6 @@ impl App {
 #[derive(Clone, Debug, Default)]
 struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
-    physical_device: vk::PhysicalDevice
+    physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue
 }
