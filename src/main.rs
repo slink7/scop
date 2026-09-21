f8bf033108d89ef48ad8568171b748c837dd15eb
Sebastien Cambier <scambier@student.42.fr>
2026-09-21 23:45:27 +0200

    Added KHR rendering surface & presenting queue

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ use vulkanalia::window as vk_window;
 use vulkanalia::prelude::v1_0::*;
 use vulkanalia::Version;
 use vulkanalia::vk::ExtDebugUtilsExtensionInstanceCommands;
+use vulkanalia::vk::KhrSurfaceExtensionInstanceCommands;
 
 const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
@@ -212,7 +213,8 @@ unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Resul
 
 #[derive(Copy, Clone, Debug)]
 struct QueueFamilyIndicies {
-    graphics: u32
+    graphics: u32,
+    present: u32
 }
 
 impl QueueFamilyIndicies {
@@ -224,8 +226,20 @@ impl QueueFamilyIndicies {
             .position(|p| p.queue_flags.contains(vk::QueueFlags::GRAPHICS))
             .map(|i| i as u32);
 
-        if let Some(graphics) = graphics {
-            Ok(Self { graphics })
+        let mut present = None;
+        for (index, properties) in properties.iter().enumerate() {
+            if instance.get_physical_device_surface_support_khr(
+                physical_device,
+                index as u32,
+                data.surface
+            )? {
+                present = Some(index as u32);
+                break ;
+            }
+        }
+
+        if let (Some(graphics), Some(present)) = (graphics, present) {
+            Ok(Self { graphics, present })
         } else {
             Err(anyhow!(SuitabilityError("Missing required queue families.")))
         }
@@ -236,10 +250,22 @@ unsafe fn create_logical_device(entry: &Entry, instance: &Instance, data: &mut A
     -> Result<Device> {
     let indices = QueueFamilyIndicies::get(instance, data, data.physical_device)?;
 
+    let mut unique_indices = HashSet::new();
+    unique_indices.insert(indices.graphics);
+    unique_indices.insert(indices.present);
+
     let queue_priorities = &[1.0];
-    let queue_info = vk::DeviceQueueCreateInfo::builder()
-        .queue_family_index(indices.graphics)
-        .queue_priorities(queue_priorities);
+    // let queue_info = vk::DeviceQueueCreateInfo::builder()
+    //     .queue_family_index(indices.graphics)
+    //     .queue_priorities(queue_priorities);
+    let queue_infos = unique_indices
+        .iter()
+        .map(|i| {
+            vk::DeviceQueueCreateInfo::builder()
+                .queue_family_index(*i)
+                .queue_priorities(queue_priorities)
+        })
+        .collect::<Vec<_>>();
 
     let layers = if VALIDATION_ENABLED {
         vec![VALIDATION_LAYER.as_ptr()]
@@ -255,10 +281,10 @@ unsafe fn create_logical_device(entry: &Entry, instance: &Instance, data: &mut A
 
     let features = vk::PhysicalDeviceFeatures::builder();
     
-    let queue_infos = &[queue_info];
+    // let queue_infos = &[queue_info];
 
     let info = vk::DeviceCreateInfo::builder()
-        .queue_create_infos(queue_infos)
+        .queue_create_infos(&queue_infos)
         .enabled_layer_names(&layers)
         .enabled_extension_names(&extensions)
         .enabled_features(&features);
@@ -266,6 +292,7 @@ unsafe fn create_logical_device(entry: &Entry, instance: &Instance, data: &mut A
     let device = instance.create_device(data.physical_device, &info, None)?;
 
     data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+    data.present_queue = device.get_device_queue(indices.present, 0);
 
     Ok(device)
 }
@@ -286,6 +313,8 @@ impl App {
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
         let mut data = AppData::default();
         let instance = create_instance(window, &entry, &mut data)?;
+
+        data.surface = vk_window::create_surface(&instance, &window, &window)?;
 
         pick_physical_device(&instance, &mut data)?;
 
@@ -309,6 +338,7 @@ impl App {
             self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
 
+        self.instance.destroy_surface_khr(self.data.surface, None);
         self.instance.destroy_instance(None);
     }
 
@@ -316,7 +346,9 @@ impl App {
 
 #[derive(Clone, Debug, Default)]
 struct AppData {
+    surface: vk::SurfaceKHR,
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
-    graphics_queue: vk::Queue
+    graphics_queue: vk::Queue,
+    present_queue: vk::Queue
 }
